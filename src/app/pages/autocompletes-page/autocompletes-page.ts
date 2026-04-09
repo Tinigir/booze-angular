b2e94fe6f6dfa340695e7eee0da720d28176ac48
Mykyta_Mielkov <nikitajohnson07.02@gmail.com>
2026-04-09 11:45:35 +0200

    Defined autocompletes page

---
+++ src/app/pages/autocompletes-page/autocompletes-page.ts
@@ -0,0 +1,75 @@
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocomplete, MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { debounceTime, switchMap } from 'rxjs';
+import { CocktailsService } from '../../shared/services/cocktails-service';
+import { IngredientsService } from '../../shared/services/ingredients-service';
+
+@Component({
+  selector: 'app-autocompletes-page',
+  imports: [
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatAutocomplete,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './autocompletes-page.html',
+  styleUrl: './autocompletes-page.scss',
+})
+export class AutocompletesPage implements OnInit {
+  destroyRef = inject(DestroyRef);
+  cocktailsService = inject(CocktailsService);
+  ingredientsServise = inject(IngredientsService);
+
+  cocktailFilteredOptions = signal<string[]>([]);
+  cocktailsSearchControl = new FormControl<string | null>(null);
+  ingredientsFilteredOptions = signal<string[]>([]);
+  ingredientsSearchControl = new FormControl<string | null>(null);
+
+  ngOnInit(): void {
+    this.listenCocktailsSearchControlChanges();
+	this.listenIngredientsSearchControlChanges();
+  }
+
+  listenIngredientsSearchControlChanges(): void {
+    this.ingredientsSearchControl.valueChanges.pipe(
+      takeUntilDestroyed(this.destroyRef),
+      debounceTime(200),
+	  switchMap((searchIngredient)=>{
+		return this.ingredientsServise.getAutocompleteIngredientsOptions(searchIngredient ?? '');
+	  }),
+    )
+	.subscribe((ingredientsResponce)=>{
+		this.ingredientsFilteredOptions.set(
+			ingredientsResponce.data.map((ingredient)=>{
+				return ingredient.name;
+			})
+		)
+	})
+  }
+
+  listenCocktailsSearchControlChanges(): void {
+    this.cocktailsSearchControl.valueChanges
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        debounceTime(200),
+        switchMap((search) => {
+          return this.cocktailsService.getAutocompleteOptions(search ?? '');
+        }),
+      )
+      .subscribe((cocktailsResponse) => {
+        this.cocktailFilteredOptions.set(
+          cocktailsResponse.data.map((cocktail) => {
+            return cocktail.name;
+          }),
+        );
+        console.log(cocktailsResponse);
+      });
+  }
+}
---
+++ src/app/shared/services/cocktails-service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { ICocktailMinimalDTO } from '../models/cocktail-minimal-dto.intarface';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { IPaginatedResponse } from '../models/generic/pagination.interface';
+import { ICocktailsFilter } from '../../pages/cocktails-page/cocktails-page';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CocktailsService {
+  http = inject(HttpClient);
+
+  getCocktailsDetails(id: string | number): Observable<ICocktailMinimalDTO> {
+    return this.http.get<ICocktailMinimalDTO>(`${environment.boozeApiPath}/cocktails/${id}`);
+  }
+
+  getCocktails(
+    page: number,
+    limit: number,
+    filters: ICocktailsFilter,
+    sort: string | null,
+    type: 'common' | 'popular' | 'latest' = 'common',
+  ): Observable<IPaginatedResponse<ICocktailMinimalDTO>> {
+    const params: any = {
+      page,
+      limit,
+    };
+
+	const endpoint = type === 'common' ? '/cocktails' : `/cocktails/${type}`;
+
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value !== null && value !== 'null') {
+        params[key] = value.toString();
+      }
+    });
+
+    if (sort) {
+      params.sort = sort;
+    }
+
+    return this.http.get<IPaginatedResponse<ICocktailMinimalDTO>>(
+      `${environment.boozeApiPath}${endpoint}`,
+      { params },
+    );
+  }
+
+  getAutocompleteOptions(q: string): Observable<IPaginatedResponse<ICocktailMinimalDTO>> {
+    const params = {
+      page: 1,
+      limit: 10,
+      q,
+    };
+    return this.http.get<IPaginatedResponse<ICocktailMinimalDTO>>(
+      `${environment.boozeApiPath}/cocktails/autocomplete`,
+      { params },
+    );
+  }
+}
