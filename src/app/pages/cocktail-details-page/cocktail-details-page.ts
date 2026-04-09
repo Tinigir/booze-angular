16edc267fa63945908e240f9b1c564f4f8e48685
Mykyta_Mielkov <nikitajohnson07.02@gmail.com>
2026-04-09 11:49:56 +0200

    Defined cocktails page, cocktails details page

---
+++ src/app/pages/cocktail-details-page/cocktail-details-page.ts
@@ -0,0 +1,44 @@
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
+import { CocktailsService } from '../../shared/services/cocktails-service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ICocktailMinimalDTO } from '../../shared/models/cocktail-minimal-dto.intarface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-cocktail-details-page',
+  imports: [RouterLink],
+  templateUrl: './cocktail-details-page.html',
+  styleUrl: './cocktail-details-page.scss',
+})
+export class CocktailDetailsPage implements OnInit {
+  cocktailsService = inject(CocktailsService);
+  destroyRef = inject(DestroyRef);
+  activatedRoute = inject(ActivatedRoute);
+
+  isLoading = signal<boolean>(false);
+  errorMessage = signal<string | null>(null);
+  cocktailSignal = signal<ICocktailMinimalDTO | null>(null);
+
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((params) => {
+      this.loadIngredientDetails(params['id']);
+      this.loadRelatedCocktails(params['id']);
+      console.log(params);
+    });
+  }
+
+  loadRelatedCocktails(id: string | number): void {
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+  }
+
+  loadIngredientDetails(id: string | number): void {
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+
+    this.cocktailsService.getCocktailsDetails(id).subscribe((data) => {
+      this.cocktailSignal.set(data);
+      console.log(data);
+    });
+  }
+}
---
+++ src/app/shared/models/cocktail-minimal-dto.intarface.ts
@@ -0,0 +1,15 @@
+import { ICategory } from './category.interface';
+import { IGlassType } from './glass-type.interface';
+import { IIngredientFullDTO } from './ingredient-full-dto.intarface';
+
+export interface ICocktailMinimalDTO {
+  alcoholic: boolean;
+  category: ICategory;
+  created_at: string;
+  glass_type: IGlassType;
+  id: number;
+  image: string;
+  ingredients: IIngredientFullDTO[];
+  name: string;
+  updated_at: string;
+}
